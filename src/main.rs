99a9056c45e24648efe3437314e44ccb6e8c533a
cod1r <jasonho353@gmail.com>
2022-12-04 14:22:54 -0600

    just thinking out things

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,59 @@ enum Ops {
     Subtract,
     Divide,
 }
-enum Expr {
+enum Literal {
+    Number(i64),
     Op(Ops),
-    Right(Box<Expr>),
-    Left(Box<Expr>),
+}
+struct Expr {
+    lit: Option<Literal>,
+    right: Option<Box<Expr>>,
+    left: Option<Box<Expr>>,
 }
 enum Token {
     Number(i64),
     Operator(Ops),
     OpenParenth,
     CloseParenth,
+}
+impl Expr {
+    fn new() -> Expr {
+        Expr { lit: None, left: None, right: None }
+    }
+}
+impl Clone for Expr {
+    fn clone(&self) -> Self {
+        Expr {
+            lit: self.lit.clone(),
+            left: self.left.clone(),
+            right: self.right.clone(),
+        }
+    }
+}
+impl Clone for Literal {
+    fn clone(&self) -> Self {
+        match self {
+            Literal::Number(num) => Literal::Number(*num),
+            Literal::Op(op) => match op {
+                Ops::Add => Literal::Op(Ops::Add),
+                Ops::Subtract => Literal::Op(Ops::Subtract),
+                Ops::Multiply => Literal::Op(Ops::Multiply),
+                Ops::Exponent => Literal::Op(Ops::Multiply),
+                Ops::Divide => Literal::Op(Ops::Divide),
+            },
+        }
+    }
+}
+impl Clone for Ops {
+    fn clone(&self) -> Self {
+        match self {
+            Ops::Add => Ops::Add,
+            Ops::Subtract => Ops::Subtract,
+            Ops::Multiply => Ops::Multiply,
+            Ops::Exponent => Ops::Multiply,
+            Ops::Divide => Ops::Divide,
+        }
+    }
 }
 fn math_lexer(math_expr: &str) -> Result<Vec<Token>, &str> {
     let math_expr_bytes = math_expr.as_bytes();
@@ -60,20 +103,73 @@ fn math_lexer(math_expr: &str) -> Result<Vec<Token>, &str> {
             } else if c == '-' {
                 tokens.push(Token::Operator(Ops::Subtract));
             }
+        } else {
+            return Err("Unknown token");
         }
         idx += 1;
     }
     Ok(tokens)
 }
-fn math_parse(tokens: Vec<Token>) {}
-fn main() {
-    let args = env::args();
-    if args.len() != 2 {
-        println!("Please offer only one CLI argument which should be a math expression");
+/*
+ * BINARYEXPR -> OPENPARENTH BINARYEXPR CLOSEPARENTH | BINARYEXPR OPERATOR BINARYEXPR | NUMBER
+ * OPERATOR -> * | - | ^ | /  */
+fn math_parse(tokens: &Vec<Token>, start: &mut usize, current: &mut usize, expr: &mut Expr) {
+    if *current >= tokens.len() {
         return;
     }
-    let args_vec = args.collect::<Vec<String>>();
-    let tokens = math_lexer(&args_vec[1]);
+    match &tokens[*current] {
+        Token::OpenParenth => {
+            *start = *current;
+            let mut local_e = Expr::new();
+            math_parse(tokens, start, current, expr);
+        }
+        Token::Number(num) => {
+            let local_e = Expr {
+                lit: Some(Literal::Number(*num)),
+                left: None,
+                right: None,
+            };
+            loop {
+                if *current >= tokens.len() {
+                    println!("expected operator.");
+                    return;
+                }
+                match &tokens[*current] {
+                    Token::Operator(op) => {
+                        *current += 1;
+                        let mut right_e = Expr::new();
+                        math_parse(tokens, start, current, &mut right_e);
+                        match right_e.lit {
+                            Some(Literal::Number(_)) => {
+                                *expr = Expr {
+                                    lit: Some(Literal::Op(op.clone())),
+                                    left: Some(Box::new(local_e.clone())),
+                                    right: Some(Box::new(right_e.clone())),
+                                };
+                            }
+                            Some(Literal::Op(_)) => {
+                                *expr = Expr {
+                                    lit: Some(Literal::Op(op.clone())),
+                                    left: Some(Box::new(right_e.clone())),
+                                    right: Some(Box::new(local_e.clone())),
+                                };
+                            }
+                            None => {}
+                        }
+                    }
+                    _ => {
+                        println!("expected operator.");
+                        return;
+                    }
+                }
+                *current += 1;
+            }
+        }
+        Token::CloseParenth => {}
+        Token::Operator(op) => {}
+    }
+}
+fn print_tokens(tokens: &Result<Vec<Token>, &str>) {
     match tokens {
         Ok(vec_tokens) => {
             for t in vec_tokens {
@@ -88,9 +184,7 @@ fn main() {
                             Multiply => println!("Multiply"),
                         }
                     }
-                    Token::Number(num) => {
-                        println!("{num}");
-                    }
+                    Token::Number(num) => println!("{num}"),
                     Token::CloseParenth => println!("CloseParenth"),
                     Token::OpenParenth => println!("OpenParenth"),
                 }
@@ -98,4 +192,14 @@ fn main() {
         }
         Err(_) => {}
     }
+}
+fn main() {
+    let args = env::args();
+    if args.len() != 2 {
+        println!("Please offer only one CLI argument which should be a math expression");
+        return;
+    }
+    let args_vec = args.collect::<Vec<String>>();
+    let tokens = math_lexer(&args_vec[1]);
+    print_tokens(&tokens);
 }
