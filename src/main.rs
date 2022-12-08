cc956291b52da0d7f191e46dfaadcad35487fd46
cod1r <jasonho353@gmail.com>
2022-12-08 17:34:22 -0600

    Redefined grammar. Shorted math_parse function. Added tests for parentheses. Errors no longer stop REPL. math_parse function now can handle parentheses correctly.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ struct Expr {
     lit: Option<Literal>,
     right: Option<Box<Expr>>,
     left: Option<Box<Expr>>,
+    precedence: u8,
 }
 enum Token {
     Number(i64),
@@ -27,6 +28,7 @@ impl Expr {
             lit: None,
             left: None,
             right: None,
+            precedence: 0,
         }
     }
 }
@@ -36,6 +38,7 @@ impl Clone for Expr {
             lit: self.lit.clone(),
             left: self.left.clone(),
             right: self.right.clone(),
+            precedence: self.precedence,
         }
     }
 }
@@ -126,11 +129,89 @@ fn get_precedence(op: &Ops) -> u8 {
     }
 }
 /*
+ *
+ * Old grammar:
  * BINARYEXPR -> OPENPARENTH BINARYEXPR CLOSEPARENTH | BINARYEXPR OPERATOR BINARYEXPR | NUMBER
- * OPERATOR -> * | - | ^ | /  */
+ * OPERATOR -> * | - | ^ | /
+   Some more advice
+   - Expr could be an enum
+   - manual Clone impls could be derived
+   - 'x' as u8 == b'x'
+   - if-else if chains can be match blocks
+   - math_lexer should take &[u8] for the input
+   - you should really assign math_expr_bytes[idx] to a variable since you write it tens of times
+   - as char can be char::from
+   - your grammar definition is left recursive
+   - to be honest I didn't read the parser too much. it seems really complicated
+   - traverse_expr_tree looks fine except for the fact that your definition of Expr makes it inelegant
+   - your print method could be replaced with a Debug impl (derived) on Token
+   - you terminate the whole repl when an expr fails to parse. I think it would be better to print an error but continue reading.
+   - on the other hand, just unwrap the IO results. no need to wory about them and breaking from the loop is kinda confusing honestly especially since you don't print the error
+
+   Some advice I got from discord on defining grammar
+       expr0 = expr1 (binary_operator expr1)*
+       expr1 = unary_operator* expr2
+       expr2 = OPEN_PAREN expr0 CLOSE_PAREN | NUMBER
+       // define unary_ and binary_operator as necessary
+
+
+**/
+fn reconcile_trees(left: &mut Expr, right: &mut Expr) {
+    // loop until we find the right place to put expr. Like
+    // a BST (Binary Search Tree)
+    let first_op_precedence = left.precedence;
+    let mut top_right = Box::new(right.clone());
+    let mut current_expr = &mut top_right;
+    loop {
+        match current_expr.lit {
+            Some(Literal::Op(_)) => {
+                let second_op_precedence = current_expr.precedence;
+                if first_op_precedence < second_op_precedence {
+                    left.right = Some(current_expr.clone());
+                    *current_expr = Box::new(left.clone());
+                    *left = *top_right.clone();
+                    break;
+                } else if second_op_precedence <= first_op_precedence {
+                    match current_expr.left {
+                        Some(ref mut left) => {
+                            current_expr = left;
+                        }
+                        None => {
+                            left.right = current_expr.left.clone();
+                            *current_expr = Box::new(left.clone());
+                            *left = *top_right.clone();
+                            break;
+                        }
+                    }
+                }
+            }
+            Some(Literal::Number(current_num)) => {
+                left.right = Some(Box::new(Expr {
+                    lit: Some(Literal::Number(current_num)),
+                    left: None,
+                    right: None,
+                    precedence: 0,
+                }));
+                *current_expr = Box::new(left.clone());
+                *left = *top_right.clone();
+                break;
+            }
+            None => {
+                unreachable!()
+            }
+        }
+    }
+}
+/*
+   New grammar:
+   uses Augmented Backus Naur Form
+   EXPR -> NUMBER OPERATOR EXPR | OPENPARENTH EXPR CLOSEPARENTH *(OPERATOR EXPR) | NUMBER
+   OPERATOR -> + | - | * | / | ^
+   NUMBER -> INTEGER THAT CAN FIT INTO i64
+*/
 fn math_parse(
     tokens: &Vec<Token>,
-    mut start: usize,
+    start: usize,
     mut current: usize,
     expr: &mut Expr,
 ) -> Result<(), &'static str> {
@@ -139,10 +220,60 @@ fn math_parse(
     }
     match &tokens[current] {
         Token::OpenParenth => {
-            start = current;
             let mut local_e = Expr::new();
-            math_parse(tokens, start, current + 1, &mut local_e)?;
+            math_parse(tokens, current + 1, current + 1, &mut local_e)?;
             *expr = local_e.clone();
+            let mut parentheses = Vec::new();
+            while current < tokens.len() {
+                match &tokens[current] {
+                    Token::Number(_) | Token::Operator(_) => {}
+                    Token::OpenParenth => {
+                        parentheses.push(&tokens[current]);
+                    }
+                    Token::CloseParenth => {
+                        if parentheses.len() > 0 {
+                            parentheses.pop();
+                        } else {
+                            return Err("Unexpected close parenthesis.");
+                        }
+                    }
+                }
+                current += 1;
+                if parentheses.len() == 0 {
+                    break;
+                }
+            }
+            if parentheses.len() > 0 {
+                return Err("Expected ')'");
+            }
+            if current >= tokens.len() {
+                expr.precedence = 4;
+                return Ok(());
+            }
+            match &tokens[current] {
+                Token::Operator(op) => {
+                    *expr = Expr {
+                        lit: Some(Literal::Op(op.clone())),
+                        left: Some(Box::new(expr.clone())),
+                        right: None,
+                        precedence: get_precedence(op),
+                    };
+                    current += 1;
+                    if current >= tokens.len() {
+                        return Err("expected right operand.");
+                    }
+                    let mut right_e = Expr::new();
+                    math_parse(tokens, current, current, &mut right_e)?;
+                    reconcile_trees(expr, &mut right_e);
+                }
+                Token::CloseParenth => {
+                    if start == 0 {
+                        return Err("Unexpected ')'");
+                    }
+                }
+                _ => return Err("expected operator."),
+            }
+            expr.precedence = 4;
         }
         Token::Number(num) => {
             expr.lit = Some(Literal::Number(*num));
@@ -156,78 +287,28 @@ fn math_parse(
                         lit: Some(Literal::Op(op.clone())),
                         left: Some(Box::new(expr.clone())),
                         right: None,
+                        precedence: get_precedence(op),
                     };
-                    let mut right_e = Expr::new();
-                    math_parse(tokens, start, current + 1, &mut right_e)?;
                     current += 1;
                     if current >= tokens.len() {
                         return Err("expected right operand.");
                     }
-                    match &tokens[current] {
-                        Token::OpenParenth => {
-                            expr.right = Some(Box::new(right_e.clone()));
+                    let mut right_e = Expr::new();
+                    math_parse(tokens, start, current, &mut right_e)?;
+                    match right_e.lit {
+                        Some(Literal::Op(_)) => {
+                            reconcile_trees(expr, &mut right_e);
+                        }
+                        Some(Literal::Number(right_num)) => {
+                            expr.right = Some(Box::new(Expr {
+                                lit: Some(Literal::Number(right_num)),
+                                left: None,
+                                right: None,
+                                precedence: 0,
+                            }));
                         }
                         _ => {
-                            match right_e.lit {
-                                Some(Literal::Op(_)) => {
-                                    // loop until we find the right place to put expr. Like
-                                    // a BST (Binary Search Tree)
-                                    let first_op_precedence = get_precedence(op);
-                                    let mut top_right = Box::new(right_e.clone());
-                                    let mut current_expr = &mut top_right;
-                                    loop {
-                                        match current_expr.lit {
-                                            Some(Literal::Op(ref current_op)) => {
-                                                let second_op_precedence =
-                                                    get_precedence(&current_op);
-                                                if first_op_precedence < second_op_precedence {
-                                                    expr.right = Some(current_expr.clone());
-                                                    *current_expr = Box::new(expr.clone());
-                                                    *expr = *top_right.clone();
-                                                    break;
-                                                } else if second_op_precedence
-                                                    <= first_op_precedence
-                                                {
-                                                    match current_expr.left {
-                                                        Some(ref mut left) => {
-                                                            current_expr = left;
-                                                        }
-                                                        None => {
-                                                            expr.right = current_expr.left.clone();
-                                                            *current_expr = Box::new(expr.clone());
-                                                            *expr = *top_right.clone();
-                                                            break;
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                            Some(Literal::Number(current_num)) => {
-                                                expr.right = Some(Box::new(Expr {
-                                                    lit: Some(Literal::Number(current_num)),
-                                                    left: None,
-                                                    right: None,
-                                                }));
-                                                *current_expr = Box::new(expr.clone());
-                                                *expr = *top_right.clone();
-                                                break;
-                                            }
-                                            None => {
-                                                unreachable!()
-                                            }
-                                        }
-                                    }
-                                }
-                                Some(Literal::Number(right_num)) => {
-                                    expr.right = Some(Box::new(Expr {
-                                        lit: Some(Literal::Number(right_num)),
-                                        left: None,
-                                        right: None,
-                                    }));
-                                }
-                                _ => {
-                                    return Err("expected right operand.");
-                                }
-                            }
+                            return Err("expected right operand.");
                         }
                     }
                 }
@@ -237,10 +318,7 @@ fn math_parse(
                 }
             }
         }
-        Token::CloseParenth => match &tokens[start] {
-            Token::OpenParenth => {}
-            _ => return Err("Unexpected ')'"),
-        },
+        Token::CloseParenth => return Err("Unexpected ')'"),
         Token::Operator(_) => {
             return Err("Expected left operand.");
         }
@@ -307,25 +385,6 @@ fn traverse_expr_tree(expr: &Expr) -> i64 {
         None => 0,
     }
 }
-fn print_tokens(tokens: &Vec<Token>) {
-    for t in tokens {
-        match t {
-            Token::Operator(op) => {
-                use Ops::*;
-                match op {
-                    Divide => println!("Divide"),
-                    Add => println!("Add"),
-                    Subtract => println!("Subtract"),
-                    Exponent => println!("Exponent"),
-                    Multiply => println!("Multiply"),
-                }
-            }
-            Token::Number(num) => println!("{num}"),
-            Token::CloseParenth => println!("CloseParenth"),
-            Token::OpenParenth => println!("OpenParenth"),
-        }
-    }
-}
 fn main() -> Result<(), &'static str> {
     let mut expr_str = String::new();
     loop {
@@ -338,11 +397,19 @@ fn main() -> Result<(), &'static str> {
             Ok(_) => {
                 let trimmed = expr_str.trim().to_string();
                 println!("Calculating: {}", trimmed);
-                let tokens = math_lexer(&trimmed)?;
-                let mut expr = Expr::new();
-                math_parse(&tokens, 0, 0, &mut expr)?;
-                println!("{}", traverse_expr_tree(&expr));
-                expr_str.clear();
+                let tokens_res = math_lexer(&trimmed);
+                match tokens_res {
+                    Ok(tokens) => {
+                        let mut expr = Expr::new();
+                        let res = math_parse(&tokens, 0, 0, &mut expr);
+                        match res {
+                            Ok(_) => println!("{}", traverse_expr_tree(&expr)),
+                            Err(s) => println!("{}", s),
+                        }
+                        expr_str.clear();
+                    }
+                    Err(s) => println!("{}", s),
+                }
             }
             Err(_) => break,
         }
@@ -423,6 +490,52 @@ mod tests {
         let mut expr = Expr::new();
         math_parse(&tokens, 0, 0, &mut expr)?;
         assert!(traverse_expr_tree(&expr) == 3 - 4 * 5 + 3i64.pow(2));
+        Ok(())
+    }
+    #[test]
+    fn p_four_minus_three_p_times_p_three_minus_5_p() -> Result<(), &'static str> {
+        let tokens = math_lexer(&"(4 - 3 ) * (3 - 5)".to_string())?;
+        let mut expr = Expr::new();
+        math_parse(&tokens, 0, 0, &mut expr)?;
+        assert!(traverse_expr_tree(&expr) == (4 - 3) * (3 - 5));
+        Ok(())
+    }
+    #[test]
+    fn p_p_p_one_plus_one_p_p_p() -> Result<(), &'static str> {
+        let tokens = math_lexer(&"(((1 + 1)))".to_string())?;
+        let mut expr = Expr::new();
+        math_parse(&tokens, 0, 0, &mut expr)?;
+        assert!(traverse_expr_tree(&expr) == (1 + 1));
+        Ok(())
+    }
+    #[test]
+    fn p_p_one_plus_three_p_times_p_four_plus_five_p_p() -> Result<(), &'static str> {
+        let tokens = math_lexer(&"((1 + 3) * (4 + 5))".to_string())?;
+        let mut expr = Expr::new();
+        math_parse(&tokens, 0, 0, &mut expr)?;
+        assert!(traverse_expr_tree(&expr) == ((1 + 3) * (4 + 5)));
+        Ok(())
+    }
+    #[test]
+    fn unmatched_parentheses() -> Result<(), &'static str> {
+        let tokens = math_lexer(&"((1) + 1".to_string())?;
+        let mut expr = Expr::new();
+        let res = math_parse(&tokens, 0, 0, &mut expr);
+        match res {
+            Err(_) => {}
+            Ok(_) => return Err("Unmatched parentheses was not caught"),
+        }
+        Ok(())
+    }
+    #[test]
+    fn extra_closing_parentheses() -> Result<(), &'static str> {
+        let tokens = math_lexer(&"(1 + 1))))))))))))))))))))))))))))))))))))))".to_string())?;
+        let mut expr = Expr::new();
+        let res = math_parse(&tokens, 0, 0, &mut expr);
+        match res {
+            Err(_) => {}
+            Ok(_) => return Err("Unmatched parentheses was not caught"),
+        }
         Ok(())
     }
 }
